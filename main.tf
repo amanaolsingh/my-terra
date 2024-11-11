320c8dd874a1384c0957edbc1d87ccb6a5208a56
Ubuntu <ubuntu@ip-172-31-16-205.ec2.internal>
2024-11-11 07:48:44 +0000

    hi

--- main.tf
+++ main.tf
@@ -1,23 +1,17 @@
 # AWS Provider Configuration
 provider "aws" {
-  region = "us-east-1"  # Set your preferred region
+ region = "us-east-1" # Set your preferred region
 }
-
 # EC2 Instance Configuration
-resource "aws_instance" "my_instance" {
-  ami           = "ami-0866a3c8686eaeeba"  # Replace with your desired AMI ID
-  instance_type = "t2.micro"                # Instance type, adjust as needed
-
-  tags = {
-    Name = "MyInstance"
-  }
+resource "aws_instance" "my_ec2" {
+ ami = "ami-0866a3c8686eaeeba" # Replace with actual AMI ID
+ instance_type = "t2.micro" # Free-tier eligible
+ tags = {
+ Name = "graded-project-instance"
+ }
 }
-
 # S3 Bucket Configuration
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-amayush"  # Use a globally unique bucket name
-  acl    = "private"      # Bucket ACL, 'private' is a common default
-
- 
+ bucket = "srmaji1234" # Use a globally unique bucket name
+ acl = "private"
 }
-
